4cc749e089f5fcb6799e2281d73b9f0cf380908f
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-06-19 15:20:33 +0300

    begin formik

---
+++ src/Form.js
@@ -0,0 +1,77 @@
+import {useFormik} from 'formik'
+
+
+const Form = () => {
+
+    const formik= useFormik({
+        intialValues: { 
+            nname: '111',
+            email:'',
+            amount: 0,
+            currency:'',
+            text: '',
+            terms: false
+        },
+        onSubmit: values => console.log(JSON.stringify(values,null,2))
+    })
+    
+    
+    return (
+        <form className="form" onSubmit={formik.handleSubmit}>
+            <h2>Отправить пожертвование</h2>
+            <label htmlFor="name">Ваше имя</label>
+            <input
+                id="name"
+                name="name"
+                // type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+            />
+            <label htmlFor="email">Ваша почта</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+            />
+            <label htmlFor="amount">Количество</label>
+            <input
+                id="amount"
+                name="amount"
+                type="number"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+            />
+            <label htmlFor="currency">Валюта</label>
+            <select
+                id="currency"
+                name="currency"
+                value={formik.values.currency}
+                onChange={formik.handleChange}>
+                    <option value="">Выберите валюту</option>
+                    <option value="USD">USD</option>
+                    <option value="UAH">UAH</option>
+                    <option value="RUB">RUB</option>
+            </select>
+            <label htmlFor="text">Ваше сообщение</label>
+            <textarea 
+                id="text"
+                name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
+            />
+            <label className="checkbox">
+                <input 
+                    name="terms" 
+                    type="checkbox" 
+                    value={formik.values.terms}
+                    onChange={formik.handleChange}/>
+                Соглашаетесь с политикой конфиденциальности?
+            </label>
+            <button type="submit">Отправить</button>
+        </form>
+    )
+}
+
+export default Form;
